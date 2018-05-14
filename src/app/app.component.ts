b67c5113ee0fe0de64e66bcfb03cd4a82c95d3f7
kazi <kazisyeef@gmail.com>
2018-05-14 08:46:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import {AngularFirestore,AngularFirestoreCollection,AngularFirestoreDocument} from 'angularfire2/firestore'; //import libaray
+import {Observable} from 'rxjs/Observable'; //import rxjs
+import 'rxjs/add/operator/map'              //import rxjs map
+import { post } from 'selenium-webdriver/http';
+
+interface Post{
+  title:string;
+  content:string;
+}
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  postCol:AngularFirestoreCollection<Post>;
+  posts:Observable<Post[]>;
+  //posts:any;
+
+  title:string;
+  content:string;
+
+  //postDoc:AngularFirestoreDocument<Post>;
+  //post:Observable<Post>;
+
+
+  constructor(private afs:AngularFirestore){
+
+  }
+
+  ngOnInit(){
+
+    this.postCol=this.afs.collection('posts');
+    this.posts=this.postCol.valueChanges();
+    //this.posts=this.postCol.snapshotChanges()
+    //.map(actions => {
+     // return actions.map(a => {
+      //  const data = a.payload.doc.data() as Post;
+       // const id = a.payload.doc.id;
+       // return { id,data};
+      //})
+//})
+
+
+  }
+
+  addPost(){
+    this.afs.collection('posts').add({'title':this.title,'content':this.content});
+   // this.afs.collection('posts').doc('my-custom-id').set({'title':this.title,'content':this.content}); //For seting custom generated id
+  }
+
+  //getPost(postId){
+   // this.postDoc=this.afs.doc('posts/'+postId);
+   /// this.post=this.postDoc.valueChanges();
+  //}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';                          //add import sta
+import { AngularFirestoreModule } from 'angularfire2/firestore';           //add import sta
+import {FormsModule} from '@angular/forms';
+
+
+ var firebaseConfig = {                                                    //Add firebase config of firestore
+  apiKey: "AIzaSyBUOIpGdk4ImlmExaqHdwFz8I4XXHAOoJ0",
+  authDomain: "firestore-3ad56.firebaseapp.com",
+  databaseURL: "https://firestore-3ad56.firebaseio.com",
+  projectId: "firestore-3ad56",
+  storageBucket: "",
+  messagingSenderId: "846859696517"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),                  //Add This
+    AngularFirestoreModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
